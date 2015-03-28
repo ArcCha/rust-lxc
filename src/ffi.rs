bb1daa5d29dfb6e6531877a7883af4dde2d2fc2f
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2015-03-28 21:30:54 +0100

    Added lxc constants.

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,5 +1,14 @@
 use libc::{c_char};
 
+pub const LXC_CLONE_KEEPNAME        : u8 = 1 << 0;
+pub const LXC_CLONE_KEEPMACADDR     : u8 = 1 << 1;
+pub const LXC_CLONE_SNAPSHOT        : u8 = 1 << 2;
+pub const LXC_CLONE_KEEPBDEVTYPE    : u8 = 1 << 3;
+pub const LXC_CLONE_MAYBE_SNAPSHOT  : u8 = 1 << 4;
+pub const LXC_CLONE_MAXFLAGS        : u8 = 1 << 5;
+pub const LXC_CREATE_QUIET          : u8 = 1 << 0;
+pub const LXC_CREATE_MAXFLAGS       : u8 = 1 << 1;
+
 #[link(name = "lxc")]
 extern {
   pub fn lxc_get_version() -> *const c_char;
