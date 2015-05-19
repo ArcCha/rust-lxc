5fa0231bc38fea5356a153a4d8f18eeaa6926e66
byebye <uaaabbjjkl@gmail.com>
2015-05-19 17:55:22 +0200

    Add doc description to all implemented methods

--- src/lib.rs
+++ src/lib.rs
@@ -67,7 +67,7 @@ impl LxcContainer {
   /// `config_path` - full path to optional config file. If you don't want to use any, pass empty string.
   ///
   /// # Return value
-  /// Returns `Ok(LxcContainer)` if the creation was sucessful, else `Err(&'static str)`.
+  /// Returns `Ok(LxcContainer)` if the creation was sucessful, else `Err(&'static str)` with error description.
   ///
   /// # Examples
   /// ```
@@ -96,42 +96,73 @@ impl LxcContainer {
     }
   }
 
+  /// Determine if /var/lib/lxc/$name/config exists.
+  ///
+  /// # Return value
+  /// Returns `true` if container is defined, else `false`.
   pub fn is_defined(&self) -> bool {
     unsafe {
       ((*self.container).is_defined)(self.container) != 0
     }
   }
 
+  /// Determine state of container.
+  ///
+  /// # Return value
+  /// Returns upper-case string representing state of container.
   pub fn state(&self) -> String { // maybe define an enum with possible states instead returning String?
     unsafe {
       ptr_to_str(((*self.container).state)(self.container))
     }
   }
 
+  /// Determine if container is running.
+  ///
+  /// # Return value
+  /// Returns `true` when container is running, else `false`.
   pub fn is_running(&self) -> bool {
     unsafe {
       ((*self.container).is_running)(self.container) != 0
     }
   }
 
+  /// Freeze running container.
+  ///
+  /// # Return value
+  /// Returns `true` on success, else `false`.
   pub fn freeze(&self) -> bool {
     unsafe {
       ((*self.container).freeze)(self.container) != 0
     }
   }
 
+  /// Thaw a frozen container.
+  ///
+  /// # Return value
+  /// Returns `true` on success, else `false`.
   pub fn unfreeze(&self) -> bool {
     unsafe {
       ((*self.container).unfreeze)(self.container) != 0
     }
   }
 
+  /// Determine process ID of the containers init process.
+  ///
+  /// # Return value
+  /// Returns pid of init process as seen from outside the container.
   pub fn init_pid(&self) -> i32 {
     unsafe {
       ((*self.container).init_pid)(self.container)
     }
   }
 
+  /// Load the specified configuration for the container.
+  ///
+  /// # Parameters
+  /// `config_path` - full path to alternate configuration file, or empty string to use the default configuration file. 
+  ///
+  /// # Return value
+  /// Returns `true` on success, else `false`.
   pub fn load_config(&self, config_path: &str) -> bool {
     unsafe {
       let mut config_path_ptr = ptr::null::<libc::c_char>();
@@ -142,6 +173,15 @@ impl LxcContainer {
     }
   }
 
+  /// Start the container.
+  ///
+  /// # Parameters
+  /// `use_init` - use lxcinit rather than /sbin/init.
+  ///
+  /// `argv` - vector of arguments to pass to init.
+  ///
+  /// # Return value
+  /// Returns `true` on success, else `false`.
   pub fn start(&self, use_init: i32, argv: Vec<&str>) -> bool {
     unsafe {
       let mut argv_ptr = ptr::null::<*const libc::c_char>();
@@ -155,60 +195,129 @@ impl LxcContainer {
     }
   }
 
+  /// Stop the container.
+  ///
+  /// # Return value
+  /// Returns `true` on success, else `false`.
   pub fn stop(&self) -> bool {
     unsafe {
       ((*self.container).stop)(self.container) != 0
     }
   }
 
+  /// Determine if the container wants to run disconnected from the terminal.
+  /// 
+  /// # Parameters
+  /// state - value for the `daemonize` bit.
+  /// 
+  /// # Return value
+  /// Returns `true` if container wants to be daemonized, else `false`.
   pub fn want_daemonize(&self, state: bool) -> bool {
     unsafe {
       ((*self.container).want_daemonize)(self.container, state as libc::c_char) != 0
     }
   }
 
+  /// Determine whether container wishes all file descriptors to be closed on startup.
+  ///
+  /// # Parameters
+  /// state - value for the `close_all_fds` bit
+  ///
+  /// # Return value
+  /// Returns `true` if container wants all file descriptors closed, else `false`.
   pub fn want_close_all_fds(&self, state: bool) -> bool {
     unsafe {
       ((*self.container).want_close_all_fds)(self.container, state as libc::c_char) != 0
     }
   }
 
+  /// Return current config file name.
+  ///
+  /// # Return value
+  /// Returns config file name.
   pub fn config_file_name(&self) -> String {
     unsafe {
+      // TODO returns NULL on error
       ptr_to_str(((*self.container).config_file_name)(self.container))
     }
   }
 
+  /// Wait for container to reach a particular state.
+  ///
+  /// # Parameters
+  /// `state` - state to wait for.
+  ///
+  /// `timeout` - timeout in seconds.
+  ///
+  /// # Return value
+  /// Returns `true` if state reached within timeout, else `false`.
   pub fn wait(&self, state: &str, timeout: i32) -> bool {
     unsafe {
       ((*self.container).wait)(self.container, str_to_ptr(state), timeout as libc::c_int) != 0
     }
   }
 
+  /// Set a key/value configuration option.
+  ///
+  /// # Parameters
+  /// `key` - name of option to set.
+  ///
+  /// `value` - value of `name` to set.
+  /// 
+  /// # Return value
+  /// Returns `true` on success, else `false`.
   pub fn set_config_item(&self, key: &str, value: &str) -> bool {
     unsafe {
       ((*self.container).set_config_item)(self.container, str_to_ptr(key), str_to_ptr(value)) != 0
     }
   }
 
+  /// Delete the container.
+  ///
+  /// ## Note
+  /// Container must be stopped and have no dependent snapshots.
+  /// 
+  /// # Return value
+  /// Returns `true` on success, else `false`.
   pub fn destroy(&self) -> bool {
     unsafe {
       ((*self.container).destroy)(self.container) != 0
     }
   }
 
+  /// Delete the container and all its snapshots.
+  ///
+  /// ## Note
+  /// Container must be stopped.
+  /// 
+  /// # Return value
+  /// Returns `true` on success, else `false`.
   pub fn destroy_with_snapshots(&self) -> bool {
     unsafe {
       ((*self.container).destroy_with_snapshots)(self.container) != 0
     }
   }
 
+  /// Save configuaration to a file.
+  ///
+  /// # Parameters
+  /// `alt_file` - full path to file to save configuration in.
+  ///
+  /// # Return value
+  /// Returns `true` on success, else `false`.
   pub fn save_config(&self, alt_file: &str) -> bool {
     unsafe {
       ((*self.container).save_config)(self.container, str_to_ptr(alt_file)) != 0
     }
   }
 
+  /// Rename a container.
+  ///
+  /// # Parameters
+  /// `new_name` - new name to be used for the container.
+  ///
+  /// # Return value
+  /// Returns `true` on success, else `false`.
   pub fn rename(&self, new_name: &str) -> bool {
     unsafe {
       ((*self.container).rename)(self.container, str_to_ptr(new_name)) != 0
